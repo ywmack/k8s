ef6ecfbec20c53553d3bce99541bcfe935b1127e
ywmack <463022467@qq.com>
2019-05-09 11:23:18 +0800

    Update down-node-images.sh

--- down-node-images.sh
+++ down-node-images.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 KUBE_VERSION=v1.12.1
 KUBE_PAUSE_VERSION=3.1
+DASHBOARD_VERSION=v1.10.1
 
-ANJIA_URL=ywmack/
+YWMACK_URL=ywmack/google-k8s
 GCR_UR=k8s.gcr.io
  
 images=(kube-proxy:${KUBE_VERSION}
-pause:${KUBE_PAUSE_VERSION})
+pause:${KUBE_PAUSE_VERSION}
+kubernetes-dashboard-amd64:${DASHBOARD_VERSION})
 
 for imageName in ${images[@]} ; do
-  docker pull $ANJIA_URL$imageName
-  docker tag $ANJIA_URL$imageName $GCR_UR/$imageName
-  docker rmi $ANJIA_URL$imageName
+  docker pull $YWMACK_URL.$imageName
+  docker tag $YWMACK_URL.$imageName $GCR_UR/$imageName
+  docker rmi $YWMACK_URL.$imageName
 done
